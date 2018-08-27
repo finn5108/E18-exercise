38cf4fe6e8f48e287a60e34950ab2820a7af62d8
fdp <finn5108@stud.kea.dk>
2018-08-27 11:38:48 +0200

    Øvelse 2 lavet færdig.

--- Exc_02/script.js
+++ Exc_02/script.js
@@ -1,3 +1,25 @@
 // Øvelse 2
 
-console.log("Øvelse 2");
+console.log("Øvelse 2 / arealTest-2");
+
+// width og height angives i meter (m)
+
+let width = 20;
+let height = 10;
+let area = 0;
+
+// Area beregnes
+area = width * height;
+
+console.log("Arealet er " + area + " kvadratmeter.");
+
+
+if (area < 100) {
+    console.log("Arealet er for lille!");
+}
+else if (area >= 100 && area < 200) {
+    console.log("Arealet er passende.")
+}
+else {
+    console.log("Arealet er for stort!")
+}
