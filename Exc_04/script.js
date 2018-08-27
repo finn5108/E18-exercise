4477be14f27bf6b8db8c466000000b64976ce267
fdp <finn5108@stud.kea.dk>
2018-08-27 18:15:00 +0200

    ovelse 4 lavet faerdig.

--- Exc_04/script.js
+++ Exc_04/script.js
@@ -1,3 +1,23 @@
 // Øvelse 4
 
 console.log("Øvelse 4");
+
+
+let firstName = "";
+let surName = "";
+
+firstName = prompt("Indtast dit fornavn");
+surName = prompt("Indtast dit efternavn");
+
+
+if (firstName != null && firstName != "" && surName != null && surName != "") {
+    alert("Goddag " + firstName + " " + surName);
+}
+else if (firstName != null && firstName != "") {
+    alert("Goddag " + firstName);
+}
+else if (surName != null && surName != "") {
+    alert("Goddag " + surName);
+} else {
+    alert("Goddag min ven")
+}
