83d9caea4caf6b9a91b070e92880a60c1e68aab4
fdp <finn5108@stud.kea.dk>
2018-08-27 11:12:56 +0200

    Exc_01 færdig  Please enter the commit message for your changes. Lines starting

--- Exc_01/script.js
+++ Exc_01/script.js
@@ -1,2 +1,12 @@
 // Øvelse 1
-console.log("Øvelse 1");
+console.log("Øvelse 1 / areal-1");
+
+//length og width angives i meter (m)
+
+let length = 5;
+let width = 5;
+let areal = 0;
+
+areal = width * length;
+console.log("Arealet er " + areal + " kvadratmeter.");
+
