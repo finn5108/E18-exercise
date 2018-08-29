c0eb3145658b8e5da2663501d2450d45cc2d0b4a
fdp <finn5108@stud.kea.dk>
2018-08-29 21:53:45 +0200

    goddag-3 tifoejet til console.log

--- Exc_03/script.js
+++ Exc_03/script.js
@@ -1,3 +1,16 @@
 // Øvelse 3
 
-console.log("Øvelse 3");
+console.log("Øvelse 3 / goddag-3");
+
+
+let userName = "";
+
+userName = prompt("Indtast dit navn!");
+
+
+if (userName != null && userName != "") {
+    alert("Goddag " + userName);
+}
+else {
+    alert("Goddag min ven");
+}
